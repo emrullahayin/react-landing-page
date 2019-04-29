1d7bedc8bb3dfd12f88ef623b9c30be130a7ec0f
Emrullah <emrullahayin@gmail.com>
2019-04-30 00:10:04 +0300

    add node sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import "./assets/styles.scss";
+
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
