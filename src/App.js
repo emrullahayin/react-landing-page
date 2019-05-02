4102a4ac5645557d6dfe7ffde7de0b698f76803b
Emrullah Ayın <e.ayin@jollytur.com>
2019-05-02 16:15:44 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -8,6 +8,45 @@ import Contact from "./components/Contact";
 import "./assets/styles/main.scss";
 
 const App = () => {
+  window.onload = function() {
+    let mainNavLinks = document.querySelectorAll("nav a.nav-link");
+    let navElement = document.querySelector("nav");
+
+    mainNavLinks.forEach(link => {
+      link.addEventListener("click", event => {
+        event.preventDefault();
+        let target = document.querySelector(event.target.hash);
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      });
+    });
+
+    window.scrollY > navElement.offsetHeight &&
+      navElement.classList.add("sticky");
+
+    window.addEventListener("scroll", event => {
+      let fromTop = window.scrollY;
+      fromTop > navElement.offsetHeight
+        ? navElement.classList.add("sticky")
+        : navElement.classList.remove("sticky");
+
+      mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if (
+          section.offsetTop <= fromTop &&
+          section.offsetTop + section.offsetHeight > fromTop
+        ) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      });
+    });
+  };
+
   return (
     <div className="App">
       <Header />
