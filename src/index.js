40b16da918d3b096d4d7bea76eb363d574dd9736
Emrullah Ayın <e.ayin@jollytur.com>
2019-05-02 10:22:24 +0300

    remove jquery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap-reboot.min.css";
