8cac7725ef566cdb4e2ba43986f8296b1c30ea06
Emrullah Ayın <e.ayin@jollytur.com>
2019-05-02 16:14:05 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -8,47 +8,6 @@ import Contact from "./components/Contact";
 import "./assets/styles/main.scss";
 
 const App = () => {
-  useEffect(() => {
-    window.onload = function() {
-      let mainNavLinks = document.querySelectorAll("nav a.nav-link");
-      let navElement = document.querySelector("nav");
-
-      mainNavLinks.forEach(link => {
-        link.addEventListener("click", event => {
-          event.preventDefault();
-          let target = document.querySelector(event.target.hash);
-          target.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-          });
-        });
-      });
-
-      window.scrollY > navElement.clientHeight &&
-        navElement.classList.add("sticky");
-        
-      window.addEventListener("scroll", event => {
-        let fromTop = window.scrollY;
-        fromTop > navElement.clientHeight
-          ? navElement.classList.add("sticky")
-          : navElement.classList.remove("sticky");
-
-        mainNavLinks.forEach(link => {
-          let section = document.querySelector(link.hash);
-
-          if (
-            section.offsetTop <= fromTop &&
-            section.offsetTop + section.clientHeight > fromTop
-          ) {
-            link.classList.add("active");
-          } else {
-            link.classList.remove("active");
-          }
-        });
-      });
-    };
-  });
-
   return (
     <div className="App">
       <Header />
