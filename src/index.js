00c8dc339db8430a948cea487845914e7060f467
Emrullah Ayın <e.ayin@jollytur.com>
2019-05-03 10:34:37 +0300

    stable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import "react-app-polyfill/ie11";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap-reboot.min.css";
