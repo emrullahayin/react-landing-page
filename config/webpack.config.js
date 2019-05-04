d1f8da31394597f3cf1cfb2744dc3aca6c62cdf7
Emrullah <emrullahayin@gmail.com>
2019-05-04 22:46:04 +0300

    bundle

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -158,7 +158,7 @@ module.exports = function(webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].js' // static/js/[name].[contenthash:8].js'
+        ? 'static/js/bundle.js' // static/js/[name].[contenthash:8].js'
         : isEnvDevelopment && 'static/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,21 @@ import bgImg from "../assets/img/bg-home.jpg";
 import styled from "styled-components";
 
 const Section = styled.section`
-  padding-left: 110px;
-  padding-right: 110px;
-  padding-top: 279px;
-  padding-bottom: 271px;
+  position: relative;
+  padding-top: 138px;
+  padding-bottom: 288px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-  position: relative;
   background-attachment: fixed;
   background-image: url(${bgImg});
 
   @media (min-width: 992px) {
     max-height: 720px;
+    padding-left: 110px;
+    padding-right: 110px;
+    padding-top: 279px;
+    padding-bottom: 271px;
   }
 
   &:after {
@@ -43,11 +45,21 @@ const Section = styled.section`
 `;
 
 const HomeTitle = styled.h1`
-  font-size: 38px;
   font-weight: 300;
-  line-height: 1.39;
   text-align: center;
   color: #fff;
+  font-size: 22px;
+  line-height: 1.55;
+  margin-bottom: 23px;
+
+  @media (min-width: 992px) {
+    font-size: 38px;
+    line-height: 1.39;
+    max-width: 1100px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 0;
+  }
 `;
 
 const SubTitle = styled.h5`
@@ -56,18 +68,23 @@ const SubTitle = styled.h5`
   line-height: 1.83;
   text-align: center;
   color: #ffffff;
-  padding: 0 340px;
+
+  @media (min-width: 992px) {
+    max-width: 385px;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 const Home = () => {
   return (
     <Section id="home">
       <div className="container">
-        <HomeTitle className="item">
+        <HomeTitle>
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout.{" "}
         </HomeTitle>
-        <SubTitle className="item">
+        <SubTitle>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry.
         </SubTitle>
