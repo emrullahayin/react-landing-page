9b828a24c87aff41d492a5de9a9152b5fd0ed601
Emrullah <emrullahayin@gmail.com>
2019-05-04 11:54:17 +0300

    styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,41 @@
 import React from "react";
 import bgImg from "../assets/img/bg-home.jpg";
+import styled from "styled-components";
+
+const Section = styled.section`
+  padding-top: 138px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  background-attachment: fixed;
+  background-image: url(${bgImg});
+
+  @media (min-width: 992px) {
+    height: 100vh;
+    max-height: 720px;
+  }
+
+  &:after {
+    content: "";
+    background: linear-gradient(33deg, #8400ff, #413bff);
+    opacity: 0.9;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    top: 0;
+
+    @media (min-width: 992px) {
+      background-image: linear-gradient(62deg, #8400ff, #413bff);
+    }
+  }
+`;
 
 const Home = () => {
-  return (
-    <section
-      id="home"
-      className="bg-home"
-      style={{ backgroundImage: `url(${bgImg})` }}
-    >
-      <div className="bg-overlay" />
-    </section>
-  );
+  return <Section id="home" />;
 };
 
 export default Home;
