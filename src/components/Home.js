ec5ce20f0a1e05db0ca21fb71910e29dbb81c443
Emrullah <emrullahayin@gmail.com>
2019-05-04 21:10:45 +0300

    ui update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,21 @@
 import React from "react";
+
 import bgImg from "../assets/img/bg-home.jpg";
 import styled from "styled-components";
 
 const Section = styled.section`
-  padding-top: 138px;
+  padding-left: 110px;
+  padding-right: 110px;
+  padding-top: 279px;
+  padding-bottom: 271px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   position: relative;
   background-attachment: fixed;
   background-image: url(${bgImg});
-  
+
   @media (min-width: 992px) {
-    height: 100vh;
     max-height: 720px;
   }
 
@@ -32,10 +35,45 @@ const Section = styled.section`
       background-image: linear-gradient(62deg, #8400ff, #413bff);
     }
   }
+  .container {
+    z-index: 1;
+    user-select: none;
+    cursor: default;
+  }
+`;
+
+const HomeTitle = styled.h1`
+  font-size: 38px;
+  font-weight: 300;
+  line-height: 1.39;
+  text-align: center;
+  color: #fff;
+`;
+
+const SubTitle = styled.h5`
+  font-size: 12px;
+  font-weight: normal;
+  line-height: 1.83;
+  text-align: center;
+  color: #ffffff;
+  padding: 0 340px;
 `;
 
 const Home = () => {
-  return <Section id="home" />;
+  return (
+    <Section id="home">
+      <div className="container">
+        <HomeTitle className="item">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout.{" "}
+        </HomeTitle>
+        <SubTitle className="item">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </SubTitle>
+      </div>
+    </Section>
+  );
 };
 
 export default Home;
