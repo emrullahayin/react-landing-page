1d378af211ad43637c08e90c7e80d6cc589286b9
Emrullah Ayın <ayinemrullah@gmail.com>
2019-05-17 17:53:25 +0300

    animate

--- src/App.js
+++ src/App.js
@@ -14,47 +14,18 @@ const App = () => {
   useEffect(() => {
     AOS.init({ once: true });
 
-    let mainNavLinks = $("nav a.nav-link");
     let navElement = $("nav");
-    let scrollTop = $(window).scrollTop();
 
     $(function() {
-      activeTab(scrollTop);
-    });
-
-    mainNavLinks.on("click", function(event) {
-      event.preventDefault();
-      let target = $(event.target.hash);
-      $("html, body").animate(
-        {
-          scrollTop: target.offset().top
-        },
-        100
-      );
-    });
-
-    $(window).on("scroll", function() {
-      let fromTop = $(window).scrollTop();
-      activeTab(fromTop);
-    });
-
-    function activeTab(sTop) {
-      sTop > navElement.innerHeight()
+      $(window).scrollTop() > navElement.innerHeight()
         ? navElement.addClass("sticky")
         : navElement.removeClass("sticky");
-
-      mainNavLinks.each(function() {
-        let section = $("" + this.hash + "");
-        if (
-          section.offset().top <= sTop &&
-          section.offset().top + section.innerHeight() > sTop
-        ) {
-          $(this).addClass("active");
-        } else {
-          $(this).removeClass("active");
-        }
-      });
-    }
+    });
+    $(window).on("scroll", function() {
+      $(window).scrollTop() > navElement.innerHeight()
+        ? navElement.addClass("sticky")
+        : navElement.removeClass("sticky");
+    });
   });
 
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
 import React from "react";
 
+import { Link, animateScroll as scroll } from "react-scroll";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
 const Header = () => {
+  const scrollTo = id => e => {
+    e.preventDefault();
+    console.log("id", id);
+    scroll.scrollTo({
+      duration: 1500,
+      delay: 100,
+      smooth: "easeInOutQuint",
+      containerId: id
+    });
+  };
+
   return (
     <header>
       <Navbar bg="none" expand="lg" fixed="top">
         <Container>
-          <Navbar.Brand href="#home" aria-label="Logo" />
+          <Navbar.Brand
+            href="#home"
+            onClick={scrollTo("home")}
+            aria-label="Logo"
+          >
+            LOGO
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span />
             <span />
@@ -17,9 +35,42 @@ const Header = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Link
+                href="#"
+                className="nav-link"
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={400}
+              >
+                Home
+              </Link>
+              <Link
+                href="#"
+                className="nav-link"
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={400}
+              >
+                About
+              </Link>
+              <Link
+                href="#"
+                className="nav-link"
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={400}
+              >
+                Contact
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
